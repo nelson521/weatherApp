b9d0bea620f6c17305a95feb3bad5aba01920cab
Stevenson Nelson <sonson521@gmail.com>
2022-01-09 17:50:25 -0500

    add form api and input

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+let key = 'dffd8dc21003f443dba070601bb18e41';
+let form = document.getElementById('weatherForm');
+let cityInput = document.getElementById('cityInput');
 
+const getResponse = async () => {
+  let res = await axios.get(
+    `http://api.openweathermap.org/data/2.5/weather?q=philadelphia&units=imperial&appid=${key}`
+  );
+  console.log(res.data.main);
+};
+
+getResponse();
