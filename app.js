30f777d6c1b3b00ae11a47eaa21c613b12a6ee00
Stevenson Nelson <sonson521@gmail.com>
2022-01-11 08:30:38 -0500

    add async to event listener

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 let key = 'dffd8dc21003f443dba070601bb18e41';
 let form = document.getElementById('weatherForm');
 let cityInput = document.getElementById('cityInput');
+let weatherText = document.getElementById('weatherText');
 
-const getResponse = async () => {
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
   let res = await axios.get(
     `http://api.openweathermap.org/data/2.5/weather?q=philadelphia&units=imperial&appid=${key}`
   );
-  console.log(res.data.main);
-};
 
-getResponse();
+  let text = document.createTextNode(
+    `Todays temp is ${res.data.main.temp} but it feels like ${res.data.main.feels_like}`
+  );
+
+  weatherText.appendChild(text);
+  console.log(cityInput.value);
+});
