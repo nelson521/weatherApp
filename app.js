a7b8569178f535ee7bc3e35114cb18ebf815c378
Stevenson Nelson <sonson521@gmail.com>
2022-01-31 21:05:04 -0500

    add math method

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ let cityInput = document.getElementById('cityInput');
 let timeStamp = document.getElementById('timeStamp');
 let temp = document.getElementById('temp');
 let description = document.getElementById('description');
-let icon = document.getElementById('icon');
 
 function checkTime(i) {
   if (i < 10) {
@@ -32,29 +31,45 @@ form.addEventListener('submit', async (e) => {
   let res = await axios.get(
     `http://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=imperial&appid=${key}`
   );
-  let currentWeather = document.createTextNode(res.data.main.temp);
+  let currentWeather = document.createTextNode(Math.trunc(res.data.main.temp));
+
   let currentDescription = document.createTextNode(
     res.data.weather[0].description
   );
+
   // add text node to temp
   temp.appendChild(currentWeather);
+
   // add description
   description.appendChild(currentDescription);
 
-  console.log(currentWeather);
+  // Add img to div
+  let img = document.createElement('img');
+  // Add div
+  let div = document.createElement('div');
+  // Add class
+  div.classList = 'justify-content-end';
+  let parent = document.getElementById('parent');
+  parent.insertBefore(div, temp);
 
-  // imgIcon.src = `http://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png`;
-  // console.log(res.data.weather[0].icon);
-  // let text = document.createTextNode(
-  //   `Todays temp is ${res.data.main.temp} but it feels like ${res.data.main.feels_like}`
-  // );
+  // Add source link
+  img.src = `http://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png`;
+  // append image
+  div.appendChild(img);
+
+  // remove child node
+  if (temp.childNodes.length > 1 || description.childNodes.length > 1) {
+    temp.childNodes[0].remove();
+    description.childNodes[0].remove();
+  }
 
   // weatherText.appendChild(text);
   // // remove child node from weather text
   // if (weatherText.childNodes.length > 1) {
   //   weatherText.childNodes[0].remove();
   // }
-  console.log();
   // Clear
   cityInput.value = '';
 });
+
+// src = ;
